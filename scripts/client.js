74a596d8fc3ba150642dc58cada731e79b94d8d4
Sarah Harrington <sarah.soberg@gmail.com>
2018-06-05 23:04:53 -0500

    working on hover

--- scripts/client.js
+++ scripts/client.js
@@ -5,6 +5,12 @@ const moreAbout = document.querySelector('#about-more');
 const lessAbout = document.querySelector('#about-less');
 const moreAboutSection = document.querySelector('.more-about-section');
 const myWork = document.querySelector('.my-work');
+const portSlides = document.querySelectorAll('.port-item-slide');
+
+console.log(portSlides);
+portSlides.forEach(function (slide) {
+  slide.addEventListener('mouseenter', showPortSlide)
+})
 
 console.log();
 
@@ -17,6 +23,10 @@ function showMoreAbout() {
 function hideSections() {
   console.log('show work button clicked');
   about.style.display = 'none';
+}
+
+function showPortSlide() {
+  console.log('item hover')
 }
 
 moreAbout.addEventListener('click', showMoreAbout);
