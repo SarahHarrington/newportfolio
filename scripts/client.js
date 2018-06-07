01e5c679c46e3b1404afd4350d9d01e1bfab60bb
Sarah Harrington <sarah.soberg@gmail.com>
2018-06-07 12:10:50 -0500

    styling updaates

--- scripts/client.js
+++ scripts/client.js
@@ -3,30 +3,41 @@ console.log('javascript loaded');
 const about = document.querySelector('.about');
 const moreAbout = document.querySelector('#about-more');
 const lessAbout = document.querySelector('#about-less');
-const moreAboutSection = document.querySelector('.more-about-section');
+const moreAboutSection = document.querySelector('#more-about-section');
 const myWork = document.querySelector('.my-work');
-const portSlides = document.querySelectorAll('.port-item-slide');
+const portSlides = [...document.querySelectorAll('.portfolio-item')];
 
-console.log(portSlides);
 portSlides.forEach(function (slide) {
-  slide.addEventListener('mouseenter', showPortSlide)
+  // console.log(slide);
+  slide.querySelector('.slide-button').addEventListener('click', showPop);
+  slide.addEventListener('mouseenter', showPortSlide);
+  slide.addEventListener('mouseleave', showPortSlide);
 })
-
-console.log();
 
 function showMoreAbout() {
   console.log('button clicked');
-  moreAboutSection.classList.toggle('show');
-  moreAbout.style.display = !'none';
+  moreAboutSection.classList.toggle('not-show');
+  moreAboutSection.classList.toggle('show-more-about-section');
+  // moreAboutSection.classList.toggle('hidden');
+  moreAbout.classList.toggle('hidden');
+  lessAbout.classList.toggle('hidden');
 }
 
 function hideSections() {
-  console.log('show work button clicked');
   about.style.display = 'none';
 }
 
-function showPortSlide() {
-  console.log('item hover')
+function showPortSlide(e) {
+  // console.log(e)
+  e.target.children[0].classList.toggle('port-item-slide');
+}
+
+// function hidePortSlide(e) {
+//   e.target.children[0].style.display = 'none';
+// }
+
+function showPop(e) {
+  console.log('Show More button clicked', e)
 }
 
 moreAbout.addEventListener('click', showMoreAbout);
