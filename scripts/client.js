d9b48923b2e9d3a69e48d2f16386152a0ae38d9a
Sarah Harrington <sarah.soberg@gmail.com>
2018-06-09 18:08:56 -0500

    adding event listeners for buttons

--- scripts/client.js
+++ scripts/client.js
@@ -3,7 +3,9 @@ console.log('javascript loaded');
 const about = document.querySelector('.about');
 const portSlides = [...document.querySelectorAll('.portfolio-item')];
 const header = document.querySelector('.header');
+const homeButton = document.querySelector('.home-button');
 const myWorkButton = document.querySelector('.my-work-button');
+const aboutMeButton = document.querySelector('.about-me-button');
 const navLinks = document.querySelectorAll('.nav-link');
 const portfolio = document.querySelector('.portfolio');
 
@@ -19,11 +21,21 @@ function showPortSlide(e) {
 
 function scrollToMyWork() {
   document.getElementById("portfolio").scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
-  header.style.display = 'grid';
+  // header.style.display = 'grid';
   header.style.background = '#F8FBF7';
   navLinks.forEach((link) => {
     link.style.background = '#F8FBF7';
     link.style.color = '#19091C';
+  })
+}
+
+function scrollToAboutMe() {
+  document.getElementById("about-me").scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  header.style.display = 'grid';
+  header.style.background = '#19091C';
+  navLinks.forEach((link) => {
+    link.style.background = '#19091C';
+    link.style.color = '#F8FBF7';
   })
 }
 
@@ -37,5 +49,8 @@ function showPop(e) {
 function scrolling(e) {
   console.log(e);
 }
+
+homeButton.addEventListener('click')
 myWorkButton.addEventListener('click', scrollToMyWork);
+aboutMeButton.addEventListener('click', scrollToAboutMe);
 portfolio.addEventListener('onscroll', scrolling);
