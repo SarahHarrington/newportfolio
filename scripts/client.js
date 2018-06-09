c52e41a3208794481aa867b2be68bdc91b0d2f90
Sarah Harrington <sarah.soberg@gmail.com>
2018-06-08 22:41:11 -0500

    more styling

--- scripts/client.js
+++ scripts/client.js
@@ -1,13 +1,11 @@
 console.log('javascript loaded');
 
 const about = document.querySelector('.about');
-const moreAbout = document.querySelector('#about-more');
-const lessAbout = document.querySelector('#about-less');
-const moreAboutSection = document.querySelector('#more-about-section');
-const myWork = document.querySelector('.my-work');
 const portSlides = [...document.querySelectorAll('.portfolio-item')];
 const header = document.querySelector('.header');
+const myWork = document.querySelector('.my-work');
 const myWorkButton = document.querySelector('.my-work-button');
+const navLinks = document.querySelectorAll('.nav-link');
 
 portSlides.forEach(function (slide) {
   // console.log(slide);
@@ -16,28 +14,23 @@ portSlides.forEach(function (slide) {
   slide.addEventListener('mouseleave', showPortSlide);
 })
 
-function showMoreAbout() {
-  console.log('button clicked');
-  moreAboutSection.classList.toggle('not-show');
-  moreAboutSection.classList.toggle('show-more-about-section');
-  moreAbout.classList.toggle('hidden');
-  lessAbout.classList.toggle('hidden');
-}
-
 function hideSections() {
   about.style.display = 'none';
   header.style.background = '#F8FBF7';
 }
-
-
 
 function showPortSlide(e) {
   e.target.children[0].classList.toggle('port-item-slide');
 }
 
 function scrollToMyWork() {
-  // location.href = "#portfolio";
   document.getElementById("portfolio").scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  header.style.display = 'grid';
+  header.style.background = '#F8FBF7';
+  navLinks.forEach((link) => {
+    link.classList.remove('more-button');
+    link.classList.add('more-button-dark');
+  })
 }
 
 // function hidePortSlide(e) {
@@ -51,7 +44,5 @@ function showPop(e) {
   activePop.style.display = 'grid';
 }
 
-moreAbout.addEventListener('click', showMoreAbout);
-lessAbout.addEventListener('click', showMoreAbout)
-myWork.addEventListener('click', hideSections);
+myWork.addEventListener('click', scrollToMyWork);
 myWorkButton.addEventListener('click', scrollToMyWork);
